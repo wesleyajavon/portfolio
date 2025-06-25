f8858849965972d5642865fe1ce2d1f052936625
wesleyajavon <wesleyajavon2203@hotmail.com>
2025-06-25 00:40:58 -0400

    mobile friendly upd

--- portfolio/src/components/Hero.jsx
+++ portfolio/src/components/Hero.jsx
@@ -1,6 +1,8 @@
 // src/components/Hero.jsx
 import React from 'react';
+import { FaGithub, FaLinkedin } from 'react-icons/fa';
 import { motion } from "framer-motion";
+
 
 export default function Hero() {
     return (
@@ -8,13 +10,15 @@ export default function Hero() {
             initial={{ opacity: 0, y: -50 }}
             animate={{ opacity: 1, y: 0 }}
             transition={{ duration: 1.5, delay: 0.8, ease: "easeOut" }}
-            className="text-center"
+            className="hidden md:block text-center"
         >
-            <section id="hero" className="py-24 bg-[url('/public/background-Hero.png')] bg-[47%_120%] sm:-[47%_80%]" >
-                <a href="#projects" className="mt-41  inline-block px-6 py-2 bg-gradient-to-r from-gray-800 to-gray-500 text-white rounded hover:text-blue-300">
+            <section id="hero" className="py-24 bg-[url('/public/background-Hero.png')] bg-[47%_120%]">
+                <a href="#projects" className="mt-41  inline-block px-6 py-2 bg-gradient-to-r from-gray-800 to-gray-500 text-white rounded hover:bg-gray-800">
                     View Projects 💡
                 </a>
             </section>
+
         </motion.div>
+
     );
 }
