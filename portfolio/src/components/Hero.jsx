276f9abac2101a4e2f8d47fd94b922bd31580f92
wesleyajavon <wesleyajavon2203@hotmail.com>
2025-06-25 00:00:25 -0400

    mobile friendly upd

--- portfolio/src/components/Hero.jsx
+++ portfolio/src/components/Hero.jsx
@@ -10,7 +10,7 @@ export default function Hero() {
             transition={{ duration: 1.5, delay: 0.8, ease: "easeOut" }}
             className="text-center"
         >
-            <section id="hero" className="py-24 bg-[url('/public/background-Hero.png')] bg-[47%_120%]">
+            <section id="hero" className="py-24 bg-[url('/public/background-Hero.png')] bg-[47%_120%] sm:-[47%_80%]" >
                 <a href="#projects" className="mt-41  inline-block px-6 py-2 bg-gradient-to-r from-gray-800 to-gray-500 text-white rounded hover:text-blue-300">
                     View Projects 💡
                 </a>
